2d36e8afef4ff4e9f874d3f3233405812e830df6
jiangwei <wwpptogether@gmail.com>
2023-05-09 21:40:56 +0800

    change html title

--- spa/src/App.jsx
+++ spa/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import Drawer from '@mui/material/Drawer';
 import Box from '@mui/material/Box';
+import { useTranslation } from 'react-i18next';
 import AppBar from './AppBar'
 import Files from './Files'
 import Books from './Books'
@@ -39,9 +40,23 @@ function App() {
   const [isList] = useAtom(isListAtom)
   const [upload, setUpload] = useAtom(uploadAtom)
   const [count, setCount] = useAtom(uploadCountAtom)
+  const [lng, setLng] = useState(navigator.language);
+  const { t, i18n } = useTranslation();
   // const [count, setCount] = useState(_.size(upload));
   const uploadRef = useRef();
   uploadRef.current = upload;
+  useEffect(() => {
+    window.onlanguagechange = (event) => {
+      const l = navigator.language.split('-')[0]
+      setLng(l)
+      // console.log(`l=${l}`)
+    }
+    return () => window.onlanguagechange = null
+  }, []);
+  useEffect(() => {
+    document.title = t('html-title')
+    // console.log(`document.title=${document.title}`)
+  }, [lng]);
   useEffect(() => {
     const ws = new WebSocket(ws_uri());
     ws.onmessage = function (event) {
