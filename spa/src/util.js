839ab32c010cf65f30aca8956d4716354c24a35e
wei-from-macmini <wwpptogether@gmail.com>
2023-07-12 19:09:36 +0800

    normolize timestamp string

--- spa/src/util.js
+++ spa/src/util.js
@@ -18,7 +18,16 @@ class Util {
         return ios ? !standalone && !safari : userAgent.includes('wv');
     }
     now_str() {
-        return new Date().toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai', hour12: false })
+        // return new Date().toLocaleString('zh-CN', { timeZone: 'Asia/Shanghai', hour12: false })
+        const pad = (n,s=2) => String(n).padStart(s, '0')
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+        const hour = date.getHours();
+        const min = date.getMinutes();
+        const sec = date.getSeconds();
+        return `${pad(year,4)}-${pad(month)}-${pad(day)} ${pad(hour)}:${pad(min)}:${pad(sec)}`;
     }
     get_ext_of_file(fn) {
         return fn.slice(fn.lastIndexOf("."));
